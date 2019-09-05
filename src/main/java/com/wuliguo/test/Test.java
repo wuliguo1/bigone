3759b708cc9c9f89126d863243a9c8b335237df7
武立国 <@10.1.7.12>
2019-09-05 13:45:59 +0800

    第一次

---
+++ src/main/java/com/wuliguo/test/Test.java
@@ -0,0 +1,16 @@
+/**
+ * Create Date:2019年9月5日
+ */
+package com.wuliguo.test;
+
+/**
+ * <br>Title:TODO 类标题
+ * <br>Description:TODO 类功能描述
+ * <br>Author:武立国(87673275@qq.com)
+ * <br>Date:2019年9月5日
+ */
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
